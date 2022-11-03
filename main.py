8f73b998247393482de1b736777daf6d7465f2fd
Nastya Stepanova <Nastya.stepanova1104@yandex.ru>
2022-11-03 17:52:55 +0300

    Second

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import sys
 
 from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 import random
+from circles import Ui_Circles
 
 
-class MyWidget(QWidget):
+class MyWidget(QWidget, Ui_Circles):
     def __init__(self):
         super().__init__()
-        uic.loadUi('circles.ui', self)
+        self.setupUi(self)
         self.show_btn.clicked.connect(self.run)
         self.do_paint = False
 
@@ -27,7 +27,7 @@ class MyWidget(QWidget):
     def draw(self, qp):
         x = random.randrange(450)
         y = random.randrange(450)
-        qp.setBrush(QColor(254, 220, 3))
+        qp.setBrush(QColor(random.randrange(255), random.randrange(255), random.randrange(255)))
         a = random.randrange(400)
         qp.drawEllipse(x, y, a, a)
 
